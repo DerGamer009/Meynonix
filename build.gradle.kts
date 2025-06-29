e5c081fe42672258fcf5995cafef134111404dae
DerGamer009 <169071753+DerGamer009@users.noreply.github.com>
2025-06-30 00:13:52 +0200

    Specify Java 17 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,3 +24,12 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(17)
+}
